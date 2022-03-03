1908408f30ce48f14dfe5587f7c46850720e53f4
Nhr98 <nirinasoa.herilanto@gmail.com>
2022-03-03 12:21:20 +0300

    edit, delete single workout, clear all workout

--- script.js
+++ script.js
@@ -80,6 +80,9 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+const resetAll = document.querySelector('.reset');
+
+const list = document.querySelectorAll('.editing');
 
 class App {
   #map;
@@ -93,10 +96,20 @@ class App {
     //load data from localStorage
     this._getLocalStorage();
 
+    // modify the workout
+    this._updateWorkout();
+
+    // delete single workout
+    this._deleteSingleWorkout();
+
     // Attach event handler
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
+    resetAll.addEventListener('click', this.reset);
+
+    //display action
+    this._handleWorkoutAction();
   }
 
   _getPosition() {
@@ -190,9 +203,6 @@ class App {
       const elevation = +inputElevation.value; //might be negative number
 
       //check if data is valid
-      // !Number.isFinite(distance) ||
-      //   !Number.isFinite(duration) ||
-      //   !Number.isFinite(elevationGain)
       if (
         !validInputs(distance, duration, elevation) ||
         !allPositive(distance, duration)
@@ -268,17 +278,7 @@ class App {
             <span class="workout__unit">spm</span>
           </div>
           <div class="editing">
-            <svg
-              class="edit"
-              fill="#ffffff"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z"
-              />
-            </svg>
-            <span class="label">Edit</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32zM421.7 220.3L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3z"/></svg>
           </div>
           <div class="deleting">
             <svg
@@ -306,6 +306,21 @@ class App {
             <span class="workout__icon">⛰</span>
             <span class="workout__value">${workout.elevationGain}</span>
             <span class="workout__unit">m</span>
+          </div>
+          <div class="editing">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32zM421.7 220.3L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3z"/></svg>
+          </div>
+          <div class="deleting">
+            <svg
+              class="delete"
+              fill="#ef4444"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+            >
+              <path
+                d="M576 384C576 419.3 547.3 448 512 448H205.3C188.3 448 172 441.3 160 429.3L9.372 278.6C3.371 272.6 0 264.5 0 256C0 247.5 3.372 239.4 9.372 233.4L160 82.75C172 70.74 188.3 64 205.3 64H512C547.3 64 576 92.65 576 128V384zM271 208.1L318.1 256L271 303C261.7 312.4 261.7 327.6 271 336.1C280.4 346.3 295.6 346.3 304.1 336.1L352 289.9L399 336.1C408.4 346.3 423.6 346.3 432.1 336.1C442.3 327.6 442.3 312.4 432.1 303L385.9 256L432.1 208.1C442.3 199.6 442.3 184.4 432.1 175C423.6 165.7 408.4 165.7 399 175L352 222.1L304.1 175C295.6 165.7 280.4 165.7 271 175C261.7 184.4 261.7 199.6 271 208.1V208.1z"
+              />
+            </svg>
           </div>
         </li>
         `;
@@ -345,6 +360,155 @@ class App {
 
     this.#workouts = data;
     this.#workouts.forEach(work => this._renderWorkout(work));
+  }
+
+  _displayWorkoutInput(workout) {
+    inputType.value = workout.type;
+    inputDistance.value = workout.distance;
+    inputDuration.value = workout.duration;
+    workout.type === 'running'
+      ? (inputCadence.value = workout.cadence)
+      : (inputElevation.value = workout.elevationGain);
+  }
+
+  _getKey(e) {
+    return e.target.closest('.workout').dataset.id;
+  }
+
+  _deleteSingleWorkout() {
+    document.querySelectorAll('.deleting').forEach(workout =>
+      workout.addEventListener('click', e => {
+        // console.log(this._getKey(e));
+
+        const data = JSON.parse(localStorage.getItem('workouts')).filter(
+          w => w.id !== this._getKey(e)
+        );
+
+        localStorage.setItem('workouts', JSON.stringify(data));
+        location.reload();
+      })
+    );
+  }
+
+  _updateWorkout() {
+    document.querySelectorAll('.editing').forEach(workout =>
+      workout.addEventListener('click', e => {
+        const data = JSON.parse(localStorage.getItem('workouts'));
+        const key = this._getKey(e);
+
+        const workout = data.find(wk => wk.id === this._getKey(e));
+
+        const validInputs = (...inputs) =>
+          inputs.every(inp => Number.isFinite(inp));
+
+        const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+        if (!workout) return;
+
+        if (workout.type === 'running') {
+          this._showForm();
+
+          inputElevation
+            .closest('.form__row')
+            .classList.add('form__row--hidden');
+          inputCadence
+            .closest('.form__row')
+            .classList.remove('form__row--hidden');
+
+          this._displayWorkoutInput(workout);
+
+          form.addEventListener('submit', e => {
+            e.preventDefault();
+
+            const type = inputType.value;
+            const distance = +inputDistance.value;
+            const duration = +inputDuration.value;
+            const cadence = +inputCadence.value;
+
+            if (
+              !validInputs(distance, duration, cadence) ||
+              !allPositive(distance, duration, cadence)
+            )
+              return alert('Please check your input');
+
+            const initial = {
+              type,
+              cadence,
+              duration,
+              distance,
+              clicks: workout.clicks,
+              coords: workout.coords,
+              date: workout.date,
+              description: workout.description,
+              id: workout.id,
+              pace: duration / distance,
+            };
+
+            localStorage.setItem(
+              'workouts',
+              JSON.stringify([initial, ...data?.filter(w => w.id !== key)])
+            );
+
+            this._hideForm();
+            location.reload();
+          });
+        }
+
+        if (workout.type === 'cycling') {
+          this._showForm();
+
+          inputElevation
+            .closest('.form__row')
+            .classList.remove('form__row--hidden');
+          inputCadence.closest('.form__row').classList.add('form__row--hidden');
+
+          this._displayWorkoutInput(workout);
+
+          form.addEventListener('submit', e => {
+            e.preventDefault();
+
+            const type = inputType.value;
+            const distance = +inputDistance.value;
+            const duration = +inputDuration.value;
+            const elevationGain = +inputElevation.value;
+
+            if (
+              !validInputs(distance, duration, elevationGain) ||
+              !allPositive(distance, duration)
+            )
+              return alert('Please check your input');
+
+            const initial = {
+              type,
+              elevationGain,
+              duration,
+              distance,
+              clicks: workout.clicks,
+              coords: workout.coords,
+              date: workout.date,
+              description: workout.description,
+              id: workout.id,
+              speed: distance / (duration / 60),
+            };
+
+            localStorage.setItem(
+              'workouts',
+              JSON.stringify([initial, ...data?.filter(w => w.id !== key)])
+            );
+
+            this._hideForm();
+            location.reload();
+          });
+        }
+      })
+    );
+  }
+
+  _handleWorkoutAction() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (data.length !== 0)
+      document.querySelector('.action').classList.remove('hidden');
   }
 
   reset() {
